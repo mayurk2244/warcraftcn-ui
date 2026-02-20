4e8d8c06701b8c898e09af3fefe4e8453e615d8b
Mayur Karmarkar <mayurk2244@gmail.com>
2026-02-20 20:18:22 +0530

    fix(avatar): update avatar size variants for improved responsiveness

--- components/ui/warcraftcn/avatar.tsx
+++ components/ui/warcraftcn/avatar.tsx
@@ -14,9 +14,9 @@ const avatarVariants = cva("fantasy relative", {
       undead: "wc-avatar-undead",
     },
     size: {
-      sm: "w-24 h-24",
-      md: "w-48 h-48",
-      lg: "w-96 h-96",
+      sm: "w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-32 lg:h-32 xl:w-36 xl:h-36",
+      md: "w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 xl:w-56 xl:h-56",
+      lg: "w-40 h-40 sm:w-56 sm:h-56 md:w-72 md:h-72 lg:w-96 lg:h-96 xl:w-[30rem] xl:h-[30rem]",
     },
   },
   defaultVariants: {
